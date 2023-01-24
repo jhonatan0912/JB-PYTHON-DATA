776eca34c0b0480bc4f7dd835e8d4211be1b6a6e
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-01-24 16:33:20 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+@app.route('/')
+def hello():
+    dataDict = {
+        "name": 'Pedro',
+        "district": 'unknow',
+        "age": '19',
+        "phone": '93817234'
+    }
+
+    return dataDict
